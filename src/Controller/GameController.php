ad200a124615ca4b13b8b3e810af41443f942f88
Dan Reinders <dan.reinders@gmail.com>
2018-09-25 11:31:30 -0400

    Add simple text grid.

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -13,27 +13,12 @@ class GameController extends AbstractController
      */
     public function index()
     {
-        $imagesPath = $this->getParameter('images_directory');
-
-        $mask = $imagesPath.'/gol*.*';
-        array_map('unlink', glob($mask));
-
-        // Create a blank image and add some text
-        $image = imagecreatetruecolor(300, 300);
-        $textColor = imagecolorallocate($image, 255, 255, 255);
-        imagestring($image, 1, 50, 50, 'A Simple Text String', $textColor);
-
-        $imageName = '/gol'.time().'.png';
-        imagepng($image, $imagesPath.$imageName);
-
-        // Free up memory
-        imagedestroy($image);
-
+        $grid = "X---X---X-\nX---X---X-\nX---X---X-\nX--X---X-\nX---X---X-\nX---X---X-\nX---X---X-\nX---X---X-\nX---X---X-\nX---X---X-\nX---X---X-";
+        
         return $this->render(
           'game/index.html.twig',
           [
-            'controller_name' => 'GameController',
-            'image_name' => 'images/'.$imageName,
+            'grid' => $grid,
           ]
         );
     }
